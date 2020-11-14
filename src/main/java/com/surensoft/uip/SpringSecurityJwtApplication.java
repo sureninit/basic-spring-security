b42866272f6f177e313aa192e9cc6ee9d23e571a
surendra shrestha <surendrashrestha@surendras-MBP.satx.rr.com>
2020-11-13 23:18:27 -0600

    basic spring security

---
+++ src/main/java/com/surensoft/uip/SpringSecurityJwtApplication.java
@@ -0,0 +1,15 @@
+package com.surensoft.uip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
